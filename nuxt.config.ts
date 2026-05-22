6a6c8dc3f77e92d0bc752ae565d3d376eeef372a
Zdeňka Kameníková <111301275+Alita-oss@users.noreply.github.com>
2026-05-22 08:32:19 +0200

    chore: mongodb setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 import { fileURLToPath } from 'url';
 export default defineNuxtConfig({
     modules: ['@nuxt/eslint'],
-    //css: ['~/assets/styles/main.scss'],
 
     css: [fileURLToPath(new URL('./assets/styles/main.scss', import.meta.url))],
+
+    runtimeConfig: {
+        MONGODB_URI: process.env.MONGODB_URI,
+    },
 
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
--- server/plugins/mongodb.ts
+++ server/plugins/mongodb.ts
@@ -1,3 +1,22 @@
-export default defineNitroPlugin(() => {
-    console.log('Mongo plugin ready (not connected yet)');
+import mongoose from 'mongoose';
+
+export default defineNitroPlugin(async () => {
+    const { MONGODB_URI } = useRuntimeConfig();
+
+    try {
+        if (mongoose.connection.readyState === 0) {
+            await mongoose.connect(MONGODB_URI, {
+                dbName: 'soap',
+            });
+            console.log('✅ MongoDB connected');
+        }
+    } catch (e) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'MongoDB connection error',
+            data: {
+                rawError: e,
+            },
+        });
+    }
 });
